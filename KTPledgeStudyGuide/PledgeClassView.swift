566d1a237ac2850a464c19b9b014b9f0fd3d51f6
Beliz Yilmaz <belizbel@live.unc.edu>
2022-03-26 11:19:22 -0400

    fix

---
+++ KTPledgeStudyGuide/PledgeClassView.swift
@@ -0,0 +1,24 @@
+//
+//  PledgeClassView.swift
+//  ktp-hackathon
+//
+//  Created by Beliz Yılmaz on 3/26/22.
+//
+
+import SwiftUI
+
+struct PledgeClassView: View {
+    var body: some View {
+        Text("Welcome to the KTPledge Study Guide")
+            .padding()
+            .foregroundColor(Color.blue)
+        
+    }
+}
+
+struct PledgeClassView_Previews: PreviewProvider {
+    static var previews: some View {
+        PledgeClassView()
+    }
+}
+
