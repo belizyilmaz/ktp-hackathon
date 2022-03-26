1cf0ffd2294b9169bcd67f9196fc361c185599c0
erayala15 <erayala@unc.edu>
2022-03-26 12:27:46 -0400

    finished navigation page ui

--- KTPledgeStudyGuide/ContentView.swift
+++ KTPledgeStudyGuide/ContentView.swift
@@ -7,21 +7,78 @@
 
 import SwiftUI
 
+
+let lightestBlue = UIColor(red: 181, green: 229, blue: 247)
+let babyBlue = UIColor(red: 105, green: 193, blue: 237)
+let navy = UIColor(red: 47, green: 100, blue: 147)
+
 struct ContentView: View {
     var body: some View {
-        Text("Welcome to the KTPledge Study Guide")
-            .padding()
-            .foregroundColor(Color.blue)
         
-        VStack {
-            HStack {
-                Rectangle()
-                    .foregroundColor(Color.blue)
-                Rectangle()
-                    .foregroundColor(Color.blue)
+        ZStack {
+            
+            LinearGradient(colors: [Color(babyBlue), Color(lightestBlue)], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .ignoresSafeArea()
+            
+            VStack {
+                
+                Spacer()
+                
+                Text("Welcome to the KTPledge Study Guide")
+                    .padding()
+                    .font(.largeTitle)
+                    .multilineTextAlignment(.center)
+                    .foregroundColor(.white)
+                
+                Spacer()
+                
+                Image("logo")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(width: 200, height: 200, alignment: .center)
+                
+                Spacer()
+                
+                HStack {
+                    NavButtonView(buttonName: "Pledge Manual")
+                    NavButtonView(buttonName: "Pledge Class")
+                }
+                
+                HStack {
+                    NavButtonView(buttonName: "KTP E-Board")
+                    NavButtonView(buttonName: "Brotherhood")
+                }
+                
+                HStack {
+                    NavButtonView(buttonName: "Greek Alphabet")
+                    NavButtonView(buttonName: "Task Checklist")
+                }
+                
+                HStack {
+                    NavButtonView(buttonName: "Lineages")
+                    NavButtonView(buttonName: "Calendar")
+                }
+                
+                Spacer()
+                
             }
         }
     }
+}
+
+struct NavButtonView: View {
+    
+    var buttonName: String
+    
+    var body: some View {
+        Text(buttonName)
+            .foregroundColor(.white)
+            .frame(width: 170, height: 80)
+            .background(Color(navy))
+            .font(.title2)
+            .cornerRadius(15)
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
