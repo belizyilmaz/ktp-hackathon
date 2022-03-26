d282f5a5781e0cd96df3ebf795656c829fe4031f
Beliz Yilmaz <belizbel@live.unc.edu>
2022-03-26 10:29:56 -0400

    Initial Commit

---
+++ Shared/ktp_hackathonApp.swift
@@ -0,0 +1,17 @@
+//
+//  ktp_hackathonApp.swift
+//  Shared
+//
+//  Created by Beliz Yılmaz on 3/26/22.
+//
+
+import SwiftUI
+
+@main
+struct ktp_hackathonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
